d627ab719b81fc7608a5d074010ba8a834dfc92f
seoleon <65116017+seoleon@users.noreply.github.com>
2026-03-12 14:04:13 +0100

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,10 @@
+project = "ROI Hacking AI Automation"
+author = "ROI Hacking"
+release = "1.0"
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+html_theme = "alabaster"
